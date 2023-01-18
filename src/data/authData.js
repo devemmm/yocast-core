f15f933897e8be699c528be3d66473f00242cdfa
devemmm <emmanuel@gennxttechsol.com>
2023-01-18 17:28:59 +0200

    removing local config

--- src/data/authData.js
+++ src/data/authData.js
@@ -1,7 +1,7 @@
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://yocast.rw";
 
 export const headers = {
-  Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZhbGVuc0BnbWFpbC5jb20iLCJpYXQiOjE2NzQwNDE4MTF9.0Fw3T9dtuYOaQ0KmknLFuzUZLv_W7st3vZ4moydQs9s`,
+  Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRqbnRpdnVndXJ1endhZW1tYW51ZWxAZ21haWwuY29tIiwiaWF0IjoxNjc0MDU0NzczfQ.8wQbzR6I_ne7ljSgQvvwoHLu6l6N5ZZqa-psA6r7Crk`,
   "Content-Type": "multipart/form-data",
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
